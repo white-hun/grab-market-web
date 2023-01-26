2c839628bc7b4c51ddda32229c89225bb79ea125
white <exwhite@naver.com>
2023-01-26 21:59:24 +0900

    Update market

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js"; //index.js는 생략해도 React에서 알아서 main의 index.js를 찾는다
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent /> {/* 위에 path 경로로 들어갔을때 이 페이지를 보여주겠다 */}
+        </Route>
+        <Route exact={true} path="/product">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
