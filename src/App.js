2585641781e06bdae54962dfa1aaf6bf1cdb8bfa
white-hun <121800818+white-hun@users.noreply.github.com>
2023-01-26 17:22:53 +0900

    Update INFL

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ChildComponent from "./child.js";
-import TimerComponent from "./timer.js";
-
+import MainPageComponent from "./main/index.js"; //index.js는 생략해도 React에서 알아서 main의 index.js를 찾는다
 function App() {
-  const text = "인프런 화이팅";
-  const sayHello = function () {
-    return <h3>인프런 강의 좋아</h3>;
-  };
-  const sayHello2 = function () {
-    alert("안녕하세요");
-  };
-  return (
-    <div>
-      <h1>안녕하세요!</h1>
-      <h2>{text}</h2>
-      {sayHello()}
-      <div onClick={sayHello2}>클릭해주세요</div>
-      <TimerComponent />
-      <ChildComponent name="white" age={28} />
-      <ChildComponent name="red" age={25} />
-      <ChildComponent name="blue" age={24} />
-      <ChildComponent name="yellow" age={23} />
-    </div>
-  );
+  return <MainPageComponent />;
 }
 
 export default App;
