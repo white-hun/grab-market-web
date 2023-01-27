d5f2f62b9a21b8ad9d38403a9ba2e047ad22601d
white-hun <121800818+white-hun@users.noreply.github.com>
2023-01-27 14:21:42 +0900

    Update market

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js"; //index.js는 생략해도 React에서 알아서 main의 index.js를 찾는다
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./products";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="images/icons/logo.png" />
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
         </div>
       </div>
-      <div id="body"></div>
-      <Switch>
-        <Route exact={true} path={"/"}>
-          <MainPageComponent /> {/* 위에 path 경로로 들어갔을때 이 페이지를 보여주겠다 */}
-        </Route>
-        <Route exact={true} path="/products/:id">
-          <ProductPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPageComponent /> {/* 위에 path 경로로 들어갔을때 이 페이지를 보여주겠다 */}
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/products/index.js
+++ src/products/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -15,8 +16,28 @@ function ProductPage() {
         console.error(error);
       });
   }, []);
-  console.log(product);
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2023년 1월 27일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
